69959ced679bb7c627c01db94a460b218f58ceeb
rohitlohokar <rohitlohokar8767@gmail.com>
2025-05-24 15:24:55 +0530

    Add files via upload

---
+++ ass1.py
@@ -0,0 +1,11 @@
+num1=int(input("enter a number:"))
+num2=int(input("enter a number:"))
+addition= num1+num2
+subtraction= num1 - num2
+multiplication= num1*num2
+division= num1/num2
+print("\results:")
+print(f"addition :{addition}")
+print(f"subtraction :{subtraction}")
+print(f"multiplication :{multiplication}")
+print(f"division :{division}")
